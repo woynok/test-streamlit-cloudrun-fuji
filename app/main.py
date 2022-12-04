d7a483728cae19fe7ba7ee14f8acc08972db27b7
woynok <woynok@gmail.com>
2022-12-04 23:38:43 +0900

    GCSから動画を取得して表示

--- app/main.py
+++ app/main.py
@@ -1,5 +1,18 @@
 import streamlit as st
+from google.cloud import storage as gcs
 
 st.title('sample')
 
 st.write('aaaa')
+
+bucket_name = 'test-streamlit-cloudrun-fuji-bucket'
+project_id = "practice-docker"
+
+client = gcs.Client(project_id)
+bucket = client.get_bucket(bucket_name)
+blob = bucket.blob('dummy_movie.mp4')
+
+st.write(blob)
+file_byte = blob.download_as_bytes()
+
+st.video(file_byte)

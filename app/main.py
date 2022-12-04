2cc261b12e2a680007f9eeb943fd17fe32351d97
woynok <woynok@gmail.com>
2022-12-05 01:38:18 +0900

    GCSから読む形に修正

--- app/main.py
+++ app/main.py
@@ -8,15 +8,15 @@ st.write('aaaa')
 bucket_name = 'test-streamlit-cloudrun-fuji-bucket'
 project_id = "practice-docker"
 
-# client = gcs.Client(project_id)
-# bucket = client.get_bucket(bucket_name)
-# blob = bucket.blob('dummy_movie.mp4')
+client = gcs.Client(project_id)
+bucket = client.get_bucket(bucket_name)
+blob = bucket.blob('dummy_movie.mp4')
 
-# st.write(blob)
-# file_byte = blob.download_as_bytes()
+st.write(blob)
+file_byte = blob.download_as_bytes()
 
-video_file = open('dummy_movie.mp4', 'rb')
-file_byte = video_file.read()
+# video_file = open('dummy_movie.mp4', 'rb')
+# file_byte = video_file.read()
 
 st.video(file_byte)
 # test
